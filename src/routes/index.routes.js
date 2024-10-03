6bd86aa92396c96f374a053c4035783ecc6932de
Nickcau1203 <158166129+Nickcau1203@users.noreply.github.com>
2024-10-03 09:42:09 -0300

    refactor: adapt get method

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import candidatosRoutes from "./candidatos.routes.js";
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
     return res.status(200).send({ message: "Vai Palmeiras!" })
 });
+
+
+routes.use("/candidatos", candidatosRoutes);
 
 export default routes;
